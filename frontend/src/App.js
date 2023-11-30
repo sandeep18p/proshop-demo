1fa1eeb9fadaa9b7b897acddb663cbc8fb3515ac
sandeep18 <sandeep.pansari98@gmail.com>
2023-11-30 19:04:08 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
+    <>
     <h1>Welcome to Proshop</h1>
+    <h1>Welcome to Proshop</h1>
+    <h1>Welcome to Proshop</h1>
+    </>
   )
 }
 
